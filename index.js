6fe1866c3b624769e7a245295d1dabcf6170a86d
Ajurgs <ajjurgs5954@gmail.com>
2021-05-11 21:33:58 -0600

    added questions and required

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is your Github username? ",
+  "What is your email?",
+  "What is your Project Name? ",
+  "Enter a description of your project: ",
+  "Enter contribution guidelines:",
+  "Enter test instructions: ",
+  "choose a license to add to the project: ",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
